24a527c696abc175f748e26ff231ef4bc1206238
Matthew Garrett <mdgarrett1984@gmail.com>
2018-01-30 02:28:13 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,5 @@
-require "pry"
-
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, h), result|
-    binding pry
     h.each do |value, names|
       names.each do |name|
         result[name] ||= {}
